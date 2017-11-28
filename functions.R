37030d4596b4979d5592a26d103d62a8885c9846
Saqib Ali <docbook.xml@gmail.com>
2017-11-28 11:03:46 -0800

    cathistogram. this plots the histogram for each variable against the response variable.

--- functions.R
+++ functions.R
@@ -25,18 +25,18 @@ continous2categorical <- function(x){
   return(data.frame(out))
 }
 
-cathistplot <- function(x){
+cathistogram <- function(x){
   
   xcolumnnames <- colnames(x)
   responsecol <- ncol(x)
-
+  
   
   plot_hist <- function (column, data, response) ggplot(data, aes(x=get(column), ..count..)) +geom_bar(aes(fill=get(response)), position="dodge") + xlab(column) + scale_fill_discrete(name=response)
   
   myplots <- lapply(colnames(x), plot_hist, data = x, response=xcolumnnames[responsecol])
   myplots <- myplots[-length(myplots)]
-
+  
   grid.arrange(grobs = myplots, ncol=1)   
-
+  
 }
 
