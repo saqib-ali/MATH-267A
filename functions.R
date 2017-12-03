dc35ae8758288d9f9f19a1653073ffd7c290307c
Maham Niaz <khattak.maham@gmail.com>
2017-12-03 12:19:48 -0800

    Update functions.R

--- functions.R
+++ functions.R
@@ -51,35 +51,35 @@ count_mat<- function(df){
 }
 
 #testing for independence between two categorical variable 
-chisq.indep <- function(m, level = 0.05, digits = 4, print = TRUE) {
-  r.sum <- rowSums(m) #works faster than apply
+chisq.indep <- function (m, level = 0.05, digits = 4, print = TRUE) 
+{
+  r.sum <- rowSums(m)
   c.sum <- colSums(m)
   n <- sum(m)
-  
-  exp.ct <- outer(r.sum, c.sum, "*")/n        # to get expected counts
+  exp.ct <- outer(r.sum, c.sum, "*")/n
   res <- m - exp.ct
-  p.res <- res / sqrt(exp.ct)                 # for Pearson residual
-  X.sq <- sum(p.res^2)                        # for Pearson statistic
-  G.sq <- 2* sum(m * (log(m) - log(exp.ct)))  # for LR statistic
+  p.res <- res/sqrt(exp.ct)
+  X.sq <- sum(p.res^2)
+  G.sq <- 2 * sum(m * (log(m) - log(exp.ct)))
   df <- (nrow(m) - 1) * (ncol(m) - 1)
   c.val <- qchisq(level, df = df, lower.tail = FALSE)
-  
-  # standardized Pearson residual
-  est.se <- sqrt(exp.ct * outer((1-r.sum/n), (1-c.sum/n), "*"))
-  s.res <- res / est.se                     
-  
-  if (print) { 
+  est.se <- sqrt(exp.ct * outer((1 - r.sum/n), (1 - c.sum/n), 
+                                "*"))
+  s.res <- res/est.se
+  if (print) {
     cat("Chi-squared test of independence\n")
-    cat("  Level = ", level, ", df = ", df, ", critical value = ", round(c.val, digits), 
-        "\n", sep = "")
+    cat("  Level = ", level, ", df = ", df, ", critical value = ", 
+        round(c.val, digits), "\n", sep = "")
     cat("  X-squared = ", round(X.sq, digits), "\n", sep = "")
     cat("  G-squared = ", round(G.sq, digits), sep = "")
-    
+    if(X.sq > c.val | G.sq > c.val){
+      cat("\n", sep = "","The test statistic value is greater than critical value. We reject the null hypothesis and conclude that the two variable are not independent")
+    } else {
+      cat("\n", sep = "","The test statistic value is less than critical value. We fail to reject the null hypothesis and conclude that the two variable are independent")
+    }
   }
-  invisible(list(X.sq = X.sq, df = df, expected = exp.ct, 
-                 pearson.res = p.res, std.res = s.res))
+  
 }
-
 #creating a table with local or global odds ratios 
 odds.ratios <- function(m, type = "local") {
   nr <- nrow(m)
